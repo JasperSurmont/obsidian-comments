1e94d6544179b81243a4f6a802ca9b437953b6cc
Jasper Surmont <jasper.surmont@splynter.be>
2024-12-10 14:39:26 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,39 +1,44 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface CommentSettings {
+	leftDelim: string
+	rightDelim: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CommentSettings = {
+	leftDelim: '{{',
+	rightDelim: '}}'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const VIEW_TYPE_COMMENT = 'comment-view'
+
+export default class Comment extends Plugin {
+	settings: CommentSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Comments', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			this.activateView();
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('comment-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerView(
+			VIEW_TYPE_COMMENT,
+			(leaf) => new CommentView(leaf)
+		)
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new CommentModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -56,7 +61,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new CommentModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +71,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new CommentSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -77,6 +82,27 @@ export default class MyPlugin extends Plugin {
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
+
+	async activateView() {
+		const { workspace } = this.app;
+	
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_COMMENT);
+	
+		if (leaves.length > 0) {
+		  // A leaf with our view already exists, use that
+		  leaf = leaves[0];
+		} else {
+		  // Our view could not be found in the workspace, create a new leaf
+		  // in the right sidebar for it
+		  leaf = workspace.getRightLeaf(false);
+		  if (!leaf) return
+		  await leaf.setViewState({ type: VIEW_TYPE_COMMENT, active: true });
+		}
+	
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf);
+	  }
 
 	onunload() {
 
@@ -91,7 +117,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class CommentModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -107,10 +133,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class CommentSettingTab extends PluginSettingTab {
+	plugin: Comment;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Comment) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +147,49 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Comment delimiter left')
+			.setDesc('How do you want a comment to start left?')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('{{')
+				.setValue(this.plugin.settings.leftDelim)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.leftDelim = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+		.setName('Comment delimiter right')
+		.setDesc('How do you want a comment to end right?')
+		.addText(text => text
+			.setPlaceholder('}}')
+			.setValue(this.plugin.settings.rightDelim)
+			.onChange(async (value) => {
+				this.plugin.settings.rightDelim = value;
+				await this.plugin.saveSettings();
+			}));
+	}
+}
+
+class CommentView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf)
+	}
+
+	getViewType() {
+		return VIEW_TYPE_COMMENT
+	}
+
+	getDisplayText() {
+		return 'Comment View'
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1]
+		container.empty()
+		container.createEl('h4', {text: 'Comment View'})
+	}
+
+	async onClose() {
+
 	}
 }
