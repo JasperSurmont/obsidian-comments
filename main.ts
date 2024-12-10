d55f1a8b025346fa1f64f950987ce9e2b4065c93
Jasper Surmont <jasper.surmont@splynter.be>
2024-12-10 20:51:29 +0100

    Initial update

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
+import { App, Editor, ItemView, MarkdownPostProcessorContext, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -19,6 +19,7 @@ export default class Comment extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		this.registerMarkdownPostProcessor(this.postProcessor)
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Comments', (evt: MouseEvent) => {
@@ -50,37 +51,10 @@ export default class Comment extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new CommentModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new CommentSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	async activateView() {
@@ -90,17 +64,13 @@ export default class Comment extends Plugin {
 		const leaves = workspace.getLeavesOfType(VIEW_TYPE_COMMENT);
 	
 		if (leaves.length > 0) {
-		  // A leaf with our view already exists, use that
 		  leaf = leaves[0];
 		} else {
-		  // Our view could not be found in the workspace, create a new leaf
-		  // in the right sidebar for it
 		  leaf = workspace.getRightLeaf(false);
 		  if (!leaf) return
 		  await leaf.setViewState({ type: VIEW_TYPE_COMMENT, active: true });
 		}
 	
-		// "Reveal" the leaf in case it is in a collapsed sidebar
 		workspace.revealLeaf(leaf);
 	  }
 
@@ -115,6 +85,19 @@ export default class Comment extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	postProcessor(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+		console.log("working")
+		let callouts = el.findAll("callout")
+
+		console.log(callouts.length)
+	}
+
+	editorExtension() {
+		this.registerEditorExtension()
+	}
+
+	
 }
 
 class CommentModal extends Modal {
